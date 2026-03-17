a1f389a6f4d046159dd2d6e68f4213e899ee9f7a
Vidar Ingason <vidar@vr.is>
2026-03-17 08:40:22 +0000

    udpate oil prices

--- R/tilfallandi/2026-03-05_oliuverd-brent-fyrir-greiningu.R
+++ R/tilfallandi/2026-03-05_oliuverd-brent-fyrir-greiningu.R
@@ -6,12 +6,13 @@ brent_yahoo <- getSymbols(
   "BZ=F",
   src = "yahoo",
   from = as.Date("2020-01-01"),
-  to = Sys.Date() + 1,
+  to = Sys.Date(),
   auto.assign = FALSE
 ) |>
   as_tibble(rownames = "date") |>
-  transmute(
-    date = as.Date(date),
+  mutate(date = str_remove(date, "^X")) |>
+  mutate(
+    date = ymd(date),
     price = `BZ=F.Close`
   ) |>
   filter(!is.na(price))
